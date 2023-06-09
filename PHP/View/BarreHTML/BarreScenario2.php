84dd0ad7ac0740dfc9674eb5bd79f506b063a0c4
Charles Parsy <charles.parsy@uphf.fr>
2023-06-09 08:20:59 +0200

    BarreScenario2 Responsive

--- PHP/View/BarreHTML/BarreScenario2.php
+++ PHP/View/BarreHTML/BarreScenario2.php
@@ -1,12 +1,27 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta title="BarreScenario" charset="UTF-8">
+    <script>
+        window.onload = init;
+        function menu_f() {
+            if(menu.classList.contains('test')) {
+                menu.classList.remove('test');
+            } else {
+                menu.classList.add('test');
+            }
+        }
+        function init() {
+            let boutton = document.getElementById("boutton");
+            let menu = document.getElementsByClassName("menu");
+            boutton.addEventListener("click", menu_f);
+        }
+    </script>
+    <title></title>
 </head>
 <body>
 <?php
-require('../../Controleur/Accueil/checkSessionSessionProf.php');
+require('../../../Controleur/Accueil/checkSessionSessionProf.php');
 ?>
   <!--Le haut de la page avec l'image et le titre-->
 
@@ -16,23 +31,28 @@ require('../../Controleur/Accueil/checkSessionSessionProf.php');
             <img src="../../../View/image/logoIFSI.png" width=120 height=80 alt="leLogo" >
         </a>
         <h1>Institut de Formation aux Soins Infirmiers (IFSI)</h1>
-
-        <br>
-        <br>
         <div class="deconnexion">
-
+            <a href="../../../Controleur/Accueil/Disconnect.php">
+                <img src="../../../View/image/Deconnexion.png" class="icone" width="50" height="50" alt="Déconnexion">
+            </a>
         </div>
     </div>
-
-  <button class="button-57" onclick="document.location='../../../View/Prof/PageProf.php'"><span class="text">Accueil</span><span>Accueil</span></button>
-  <button class="button-57" onclick="document.location='../CreateScenario.php'"><span class="text">Scénario</span><span>Scénario</span></button>
-  <button class="button-57" onclick="document.location='../Correction.php'"><span class="text">Correction</span><span>Correction</span></button>
-  <button class="button-57" onclick="document.location='../AvantLesNotes.php'"><span class="text">Note</span><span>Note</span></button>
-  <button class="button-57" onclick="document.location='../../Accueil/chat.php'"><span class="text">Message</span><span>Message</span></button>
-  <br>
+    <button type="button" id="boutton" class="menu">
+        <span class="burger-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+        </span>
+    </button>
+    <div id="menu" class="test">
+        <button class="button-57" onclick="document.location='../../../View/Prof/PageProf.php'"><span class="text">Accueil</span><span>Accueil</span></button>
+        <button class="button-57" onclick="document.location='../CreateScenario.php'"><span class="text">Scénario</span><span>Scénario</span></button>
+        <button class="button-57" onclick="document.location='../Correction.php'"><span class="text">Correction</span><span>Correction</span></button>
+        <button class="button-57" onclick="document.location='../AvantLesNotes.php'"><span class="text">Note</span><span>Note</span></button>
+        <button class="button-57" onclick="document.location='../../Accueil/chat.php'"><span class="text">Message</span><span>Message</span></button>
+    </div>
     <br>
 </div>
-  </header>
 
 </body>
 </html>
